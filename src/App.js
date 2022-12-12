acf208a00dcccddf2bc4b90933da6aba3b80d422
Shang Qiao <shangqiao1996@163.com>
2022-12-12 20:13:31 +0800

    引入ant-design组件库

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+// import 'antd/dist/reset.css';
+import Header from './components/header';
+import { Button } from 'antd';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <Header />
+        <Button type='primary'>BUtton</Button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './index.less';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='header'>header</div>
+    );
+  }
+}
