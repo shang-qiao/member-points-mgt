ae18e9cbef6f6a11a5dd26a519e8ffbaeb3b0147
Shang Qiao <shangqiao1996@163.com>
2022-12-12 23:07:05 +0800

    编写header组件html&css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 // import 'antd/dist/reset.css';
 import Header from './components/header';
-import { Button } from 'antd';
 
 const App = () => {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Header />
-        <Button type='primary'>BUtton</Button>
-      </header>
+      <Header />
     </div>
   );
 };
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import './index.less';
+import './index.scss';
 
 export default class Header extends Component {
   render() {
     return (
-      <div className='header'>header</div>
+      <header className='nav'>
+        <div className='nav-row'>
+          <h1 className='title'></h1>
+          <div className='nav-group'>
+            <span className='bell'></span>
+            <span className='question'></span>
+            <span className='line'></span>
+            <span className='photo'></span>
+            <span className='name'>佚 名</span>
+            <span className='down'></span>
+          </div>
+        </div>
+      </header>
     );
+  }
+  componentDidMount() {
+
   }
 }
